09f4ac2215881eb07e7bc562bd7e2978e47a4097
Vishavpreet Sandhu <vishal1949@gmail.com>
2019-03-02 17:59:59 -0800

    Fix explore-tab margins

--- frontend/components/explore_tab.jsx
+++ frontend/components/explore_tab.jsx
@@ -7,7 +7,7 @@ class ExploreTab extends React.Component{
     render(){
         return(
             <>
-                {/* <h2>Explore</h2> */}
+                <h2>Explore</h2>
                 <Link to='/listings' className='entire-tab'>
                     <div className='link-content'>
                         <img className='explore-img' src='https://images.pexels.com/photos/279719/pexels-photo-279719.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'></img>
